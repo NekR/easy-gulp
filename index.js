54615a7354fd7d68b2c34216ba7928edc5a4ddcb
Arthur Stolyar <nekr.fabula@gmail.com>
2015-10-02 20:03:57 +0300

    init

---
+++ index.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp');
+var del = require('del');
+
+function makeDeps(deps, modifier) {
+  return gulp.series.apply(gulp, deps.map(function(dep) {
+    return dep + ':run';
+  }));
+}
+
+function getWatch(task, parent) {
+  var watch = task.watch || task.src;
+
+  return (watch ? (parent || []).concat(watch) : parent || []);
+}
+
+function make(tasks) {
+  var runList = [];
+  var watchList = [];
+
+  Object.keys(tasks).forEach(function(key) {
+    var task = tasks[key];
+
+    var runKey = key + ':run';
+    var watchKey = key + ':watch';
+    var cleanKey = key + ':clean';
+
+    if (task.clean) {
+      gulp.task(cleanKey, function(cb) {
+        del(task.clean, cb);
+      });
+    }
+
+    var run = function runWrap(cb) {
+      return task.run(cb);
+    };
+
+    gulp.task(runKey, task.deps ? gulp.series(makeDeps(task.deps), run) : run);
+
+    var watchGlobs = getWatch(task);
+
+    if (task.deps) {
+      task.deps.forEach(function(dep) {
+        var depTask = tasks[dep];
+
+        depTask.global = false;
+        watchGlobs = getWatch(depTask, watchGlobs);
+      });
+
+      task.watch = watchGlobs;
+    }
+
+    if (watchGlobs && watchGlobs.length) {
+      gulp.task(watchKey, function() {
+        gulp.watch(
+          watchGlobs,
+          task.clean ? gulp.series(cleanKey, runKey) : gulp.series(runKey)
+        );
+      });
+    }
+
+    task.runKey = runKey;
+    task.watchKey = watchKey;
+  });
+
+  Object.keys(tasks).forEach(function(key) {
+    var task = tasks[key];
+
+    if (task.global !== false) {
+      runList.push(task.runKey);
+      watchList.push(task.watchKey);
+    }
+  });
+
+  return {
+    run: runList,
+    watch: watchList
+  };
+}
+
+module.exports = make;
